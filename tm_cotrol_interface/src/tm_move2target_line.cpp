e8d4f1ed8dd951b0f1e0459755a58f52feec8067
matt60110552 <matt6011055.eed07@nctu.edu.tw>
2023-04-10 17:25:08 +0800

    delete sophia_test and tm_control_interface

--- tm_cotrol_interface/src/tm_move2target_line.cpp
+++ tm_cotrol_interface/src/tm_move2target_line.cpp
@@ -45,7 +45,6 @@
 #include "tm_driver/tm_communication.h"
 #include "tm_driver/tm_robot_state.h"
 #include "tm_driver/tm_ros_node.h"
-#include "tm_kinematics/tm_kin.h"
 
 #include  <tm_msgs/FeedbackState.h>
 
@@ -80,9 +79,6 @@ void ToolPose_Callback(const geometry_msgs::PoseStamped::ConstPtr& ToolPose)
             0., 0., 0., tm_end_effector_p[2],
             0., 0., 0., 1.;
     Eigen::Matrix<double,3,3> rot;
-    // tf::matrixTFToEigen(m,rot);
-    // T_tm_end_effector.block<3,3>(0,0) = rot.block<3,3>(0,0);
-    //Roll: rx, Pitch: ry, Yaw: rz, unit: rad
     m.getRPY(tm_end_effector_p[3], tm_end_effector_p[4], tm_end_effector_p[5]);
 
 }
@@ -95,24 +91,6 @@ void ToolPose_Callback2(const tm_msgs::FeedbackState::ConstPtr& ToolPose)
     tm_end_effector_p[3] = ToolPose->tool_pose[3];
     tm_end_effector_p[4] = ToolPose->tool_pose[4];
     tm_end_effector_p[5] = ToolPose->tool_pose[5];
-
-    // tf::Quaternion q(
-    //     ToolPose->pose.orientation.x,
-    //     ToolPose->pose.orientation.y,
-    //     ToolPose->pose.orientation.z,
-    //     ToolPose->pose.orientation.w);
-    // tf::Matrix3x3 m(q);
-
-    // T_tm_end_effector <<   0., 0., 0., tm_end_effector_p[0],
-    //         0., 0., 0., tm_end_effector_p[1],
-    //         0., 0., 0., tm_end_effector_p[2],
-    //         0., 0., 0., 1.;
-    // Eigen::Matrix<double,3,3> rot;
-    // // tf::matrixTFToEigen(m,rot);
-    // // T_tm_end_effector.block<3,3>(0,0) = rot.block<3,3>(0,0);
-    // //Roll: rx, Pitch: ry, Yaw: rz, unit: rad
-    // m.getRPY(tm_end_effector_p[3], tm_end_effector_p[4], tm_end_effector_p[5]);
-
 }
 
 void Target_Callback(const geometry_msgs::Transform::ConstPtr& TargetPose)
@@ -127,7 +105,6 @@ void Target_Callback(const geometry_msgs::Transform::ConstPtr& TargetPose)
         TargetPose->rotation.z,
         TargetPose->rotation.w);
     tf::Matrix3x3 m(q);
-    //Roll: rx, Pitch: ry, Yaw: rz, unit: rad
     m.getRPY(tm_target_p[3], tm_target_p[4], tm_target_p[5]);
 
 
@@ -139,70 +116,6 @@ void Startmove_Callback(const std_msgs::Int32::ConstPtr& startmove)
     start_cmd=startmove->data;
     ROS_ERROR("start_cmd = %d / %d",start_cmd,startmove->data);
 
-}
-
-bool CheckJointLimit(double *q){
-    bool valid = true;
-
-    if(abs(q[0]) > 270*D2R)
-    {
-        print_warn("[WARN] the 1th joint : %lf\n",q[0] );
-        valid = false;
-    }
-    else if(abs(q[1]) > 1.57)
-    {
-        print_warn("[WARN] the 2th joint : %lf\n",q[1] );
-        valid = false;
-    }
-    else if(abs(q[2]) > 155*D2R)
-    {
-        print_warn("[WARN] the 3th joint : %lf\n",q[2] );
-        valid = false;
-    }
-    else if(abs(q[3]) > 180*D2R)
-    {
-        print_warn("[WARN] the 4th joint : %lf\n",q[3] );
-        valid = false;
-    }
-    else if(abs(q[4]) > 180*D2R)
-    {
-        print_warn("[WARN] the 5th joint : %lf\n",q[4] );
-        valid = false;
-    }
-    else if(abs(q[5]) > 270*D2R)
-    {
-        print_warn("[WARN] the 6th joint : %lf\n",q[5] );
-        valid = false;
-    }
-    else
-        valid = true;
-
-    return valid;
-}
-
-bool GetQfromInverseKinematics( double* CartesianPosition, double *q_inv)
-{
-    Eigen::Matrix<float,4,4> T_;
-    Eigen::AngleAxisf yawAngle (CartesianPosition[5], Eigen::Vector3f::UnitZ());
-    Eigen::AngleAxisf pitchAngle  (CartesianPosition[4], Eigen::Vector3f::UnitY());
-    Eigen::AngleAxisf rollAngle(CartesianPosition[3], Eigen::Vector3f::UnitX());
-    Eigen::Quaternion<float> q = yawAngle * pitchAngle *rollAngle;
-    Eigen::Matrix<float,3,3> RotationMatrix = q.matrix();
-    double *T = new double[16];
-
-
-    T_ <<   0., 0., 0., CartesianPosition[0],
-            0., 0., 0., CartesianPosition[1],
-            0., 0., 0., CartesianPosition[2],
-            0., 0., 0., 1.;
-
-    T_.block<3,3>(0,0) = RotationMatrix.block<3,3>(0,0);
-
-    tm_jacobian::Matrix2DoubleArray(T_,T);
-    int num_sol =  tm_kinematics::inverse(T, q_inv);
-
-    delete [] T;
-    return CheckJointLimit(q_inv);
 }
 
 int main(int argc, char **argv)
@@ -245,60 +158,29 @@ int main(int argc, char **argv)
     // ros::Publisher pub_estimation;
     ros::Rate r(10);
     int valid=0;
-    // while(valid!=1){
     while(ros::ok()){
-        //ROS_ERROR("start_cmd = %d",start_cmd);
-        // printf("j=%lf %lf %lf %lf %lf %lf\n",tm_target_j[0],tm_target_j[1],tm_target_j[2],tm_target_j[3],tm_target_j[4],tm_target_j[5]);
-        // double T[16];
-
-        // tm_kinematics::forward(tm_target_j, T);
-
-        // cout << start_cmd<<">>>> T06" << endl;
-        // tm_jacobian::printMatrix(T,4,16);
         if(start_cmd!=0){
-            if (GetQfromInverseKinematics(tm_target_p, tm_target_j)){
-                // printf("j=%lf %lf %lf %lf %lf %lf\n",tm_target_j[0],tm_target_j[1],tm_target_j[2],tm_target_j[3],tm_target_j[4],tm_target_j[5]);
-                double T[16];
-                tm_kinematics::forward(tm_target_j, T);
-
-                cout << start_cmd<<">>>> T06" << endl;
-                tm_jacobian::printMatrix(T,4,16);
-                target_pose.request.positions.clear();
-                // std::vector<double> target={0,0,0,0,0,0};
-                
-                for(int i=0;i<6;i++){
-                // target[i]=tm_target_j[i];
-                    target_pose.request.positions.push_back(tm_target_p[i]);
-                }
-                
-                // std::cin>>valid;
-                // if(valid==-1){
-                //     target_pose.request.positions=home;
-                // }
-
-                if (client.call(target_pose))                             
-                {
-                    start_cmd=0;
-                    if (target_pose.response.ok) ROS_INFO_STREAM("SetPositions to robot");
-                    else ROS_WARN_STREAM("SetPositions to robot , but response not yet ok ");
-                }
-                else
-                {
-                    start_cmd=0;
-                    ROS_ERROR_STREAM("Error SetPositions to robot");
-                    return 1;
-                }
+            cout << start_cmd<<">>>> T06" << endl;
+            target_pose.request.positions.clear();
+            for(int i=0;i<6;i++){
+            // target[i]=tm_target_j[i];
+                target_pose.request.positions.push_back(tm_target_p[i]);
             }
-            else{
-                ROS_ERROR("out of workspace");
+
+            if (client.call(target_pose))                             
+            {
                 start_cmd=0;
+                if (target_pose.response.ok) ROS_INFO_STREAM("SetPositions to robot");
+                else ROS_WARN_STREAM("SetPositions to robot , but response not yet ok ");
             }
-            
-
+            else
+            {
+                start_cmd=0;
+                ROS_ERROR_STREAM("Error SetPositions to robot");
+                return 1;
+            }
         }
         r.sleep();
-        
     }
-    
     return 0;
 }
